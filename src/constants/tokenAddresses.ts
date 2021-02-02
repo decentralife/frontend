c642be49e9835488b23decbcafe4bd48f4e87824
Decentralife <christopherdonaldmanning@gmail.com>
2021-02-02 18:29:32 -0500

    added all

--- src/constants/tokenAddresses.ts
+++
@@ -1,4 +0,0 @@
-export const sushi = '0x0e6751e6f5da831f2e799db01ef4c8cacf4298e2'
-export const sushiv2 = '0x0e6751e6f5da831f2e799db01ef4c8cacf4298e2'
-export const sushiAddress = '0x0e6751e6f5da831f2e799db01ef4c8cacf4298e2'
-export const masterChefAddress = '0x0e6751e6f5da831f2e799db01ef4c8cacf4298e2'
