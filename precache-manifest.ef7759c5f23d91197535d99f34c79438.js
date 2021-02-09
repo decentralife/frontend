a3b955ed8c9fd823ab2e145de2322a73b10d1660
Decentralife <christopherdonaldmanning@gmail.com>
2021-02-09 18:11:00 -0500

    added

---
+++ precache-manifest.ef7759c5f23d91197535d99f34c79438.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c2a91c4e426b82a2b91",
+    "url": "./static/css/main.22a4cb7c.chunk.css"
+  },
+  {
+    "revision": "0c2a91c4e426b82a2b91",
+    "url": "./static/js/main.0c2a91c4.chunk.js"
+  },
+  {
+    "revision": "77ca7057a60af6c7850b",
+    "url": "./static/css/1.f116d424.chunk.css"
+  },
+  {
+    "revision": "77ca7057a60af6c7850b",
+    "url": "./static/js/1.77ca7057.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "./static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e0a371e78decccec11fe158c42b708e5",
+    "url": "./static/media/dai.e0a371e7.png"
+  },
+  {
+    "revision": "72ae29d4ef7d734cca53c17c6fde35aa",
+    "url": "./index.html"
+  }
+];
