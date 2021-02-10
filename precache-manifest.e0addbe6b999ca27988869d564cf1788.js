1505b64d64b7654915d7f4b288e6ef93b48c1dc4
Decentralife <christopherdonaldmanning@gmail.com>
2021-02-10 15:32:36 -0500

    updated

---
+++ precache-manifest.e0addbe6b999ca27988869d564cf1788.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f6f09bdcb7a022badab",
+    "url": "./static/js/0.5f6f09bd.chunk.js"
+  },
+  {
+    "revision": "d2ba99eca0aa512ccee2",
+    "url": "./static/css/main.2aab1c4c.chunk.css"
+  },
+  {
+    "revision": "d2ba99eca0aa512ccee2",
+    "url": "./static/js/main.d2ba99ec.chunk.js"
+  },
+  {
+    "revision": "31f5123129c80ee33206",
+    "url": "./static/css/2.9a8d6964.chunk.css"
+  },
+  {
+    "revision": "31f5123129c80ee33206",
+    "url": "./static/js/2.31f51231.chunk.js"
+  },
+  {
+    "revision": "ee6e4aa3e9475d54ebee",
+    "url": "./static/js/3.ee6e4aa3.chunk.js"
+  },
+  {
+    "revision": "0443e77bab69adebf5ca",
+    "url": "./static/js/4.0443e77b.chunk.js"
+  },
+  {
+    "revision": "d9e63033d6648002339c",
+    "url": "./static/js/5.d9e63033.chunk.js"
+  },
+  {
+    "revision": "bb6e19c3e5218289b87d",
+    "url": "./static/js/6.bb6e19c3.chunk.js"
+  },
+  {
+    "revision": "63f0dae2aa4c29ace614",
+    "url": "./static/js/7.63f0dae2.chunk.js"
+  },
+  {
+    "revision": "2c6fc2ec3a29986c6ce3",
+    "url": "./static/js/8.2c6fc2ec.chunk.js"
+  },
+  {
+    "revision": "b5e1a274419106b1a118",
+    "url": "./static/js/9.b5e1a274.chunk.js"
+  },
+  {
+    "revision": "41d409e622491289c8af",
+    "url": "./static/js/runtime~main.41d409e6.js"
+  },
+  {
+    "revision": "e0a371e78decccec11fe158c42b708e5",
+    "url": "./static/media/dai.e0a371e7.png"
+  },
+  {
+    "revision": "79004bf26c449494b1a0a46550b795f2",
+    "url": "./index.html"
+  }
+];
